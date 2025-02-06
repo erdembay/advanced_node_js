ec55be9062216b7da8385656f7bf6e17870f2a84
Erdem Bay <erdem.bay@mobilulasim.com>
2025-02-06 22:11:39 +0300

    mongo db bağlantısı yapıldı

--- app.js
+++ app.js
@@ -3,66 +3,27 @@ const express = require("express"); // express modülü eklendi
 const rootDir = require("./utils/path"); // rootDir modülü eklendi
 const app = express(); // express uygulaması oluşturuldu
 const errorController = require("./controllers/error"); // errorController modülü eklendi
-const sequelize = require("./utils/database"); // db modülü eklendi
-const Product = require("./models/product"); // Product modeli eklendi
-const Cart = require("./models/cart"); // Cart modeli eklendi
-const CartItem = require("./models/cart-item"); // CartItem modeli eklendi
-const User = require("./models/user"); // User modeli eklendi
-const Order = require("./models/order"); // Order modeli eklendi
-const OrderItem = require("./models/order-item"); // OrderItem modeli eklendi
+const mongoConnect = require("./utils/database"); // db modülü eklendi
 app.set("view engine", "ejs"); // view engine olarak ejs kullanıldı
 app.set("views", "views"); // views klasörü belirtildi
-const adminRoutes = require("./routes/admin"); // adminRoutes modülü eklendi
-const shopRoutes = require("./routes/shop"); // shopRoutes modülü eklendi
+// const adminRoutes = require("./routes/admin"); // adminRoutes modülü eklendi
+// const shopRoutes = require("./routes/shop"); // shopRoutes modülü eklendi
 app.use(express.urlencoded({ extended: false })); // urlencoded veri alışverişi için kullanıldı
 app.use(express.static(path.join(rootDir, "public"))); // public klasörüne erişim sağlandı
 app.use((req, res, next) => {
-  User.findByPk(1)
-    .then((user) => {
-      req.user = user; // user request objesine eklendi
-      next(); // sonraki middleware'e geçildi
-    })
-    .catch((err) => {
-      console.log(err); // hata konsola yazdırıldı
-    });
+  // User.findByPk(1)
+  //   .then((user) => {
+  //     req.user = user; // user request objesine eklendi
+  //     next(); // sonraki middleware'e geçildi
+  //   })
+  //   .catch((err) => {
+  //     console.log(err); // hata konsola yazdırıldı
+  //   });
 }); // middleware tanımlandı
-app.use("/admin", adminRoutes); // adminRoutes middleware olarak kullanıldı
-app.use(shopRoutes); // shopRoutes middleware olarak kullanıldı
+// app.use("/admin", adminRoutes); // adminRoutes middleware olarak kullanıldı
+// app.use(shopRoutes); // shopRoutes middleware olarak kullanıldı
 app.use(errorController.get404Page); // 404 hatası için errorController.get404 fonksiyonu kullanıldı
-Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" }); // Product modeli User modeline bağlandı
-User.hasMany(Product); // User modeli Product modeline bağlandı
-User.hasOne(Cart); // User modeli Cart modeline bağlandı
-Cart.belongsTo(User); // Cart modeli User modeline bağlandı
-Cart.belongsToMany(Product, { through: CartItem }); // Cart modeli Product modeline bağlandı
-Product.belongsToMany(Cart, { through: CartItem }); // Product modeli Cart modeline bağlandı
-Order.belongsTo(User); // Order modeli User modeline bağlandı
-User.hasMany(Order); // User modeli Order modeline bağlandı
-Order.belongsToMany(Product, { through: OrderItem }); // Order modeli Product modeline bağlandı
-sequelize
-  .sync
-  // sequelize ile sync fonksiyonu kullanıldı
-  // { force: true } // force true olarak ayarland
-  ()
-  .then((result) => {
-    // sequelize ile sync fonksiyonu kullanıldı
-    // console.log(result); // sonuç konsola yazdırıldı
-    return User.findByPk(1);
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({
-        name: "Erdem Bay",
-        email: "m.erdembay@gmail.com",
-      });
-    }
-    return user;
-  })
-  .then((user) => {
-    return user.createCart();
-  })
-  .then((cart) => {
-    app.listen(3000); // 3000 portu dinlenmeye başlandı
-  })
-  .catch((err) => {
-    console.log(err); // hata konsola yazdırıldı
-  });
+mongoConnect((client) => {
+  console.log(client); // client konsola yazdırıldı
+  app.listen(3000); // 3000 portu dinlenmeye başlandı
+}); // mongoConnect fonksiyonu çalıştırıldı
--- utils/database.js
+++ utils/database.js
@@ -1,6 +1,13 @@
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize("node-complete", "root", "12345678", {
-  dialect: "mysql",
-  host: "localhost",
-});
-module.exports = sequelize;
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient; // MongoClient modülü eklendi Ne işe yarar? MongoDB veritabanına bağlanmamızı sağlar.
+const mongoConnect = (callback) => {
+  MongoClient.connect("mongodb://localhost:27017/node-complete")
+    .then((result) => {
+      console.log("Mongo Connected!"); // bağlantı başarılı konsola yazdırıldı
+      callback(result); // callback fonksiyonu çalıştırıldı
+    })
+    .catch((err) => {
+      console.log(err); // hata konsola yazdırıldı
+    }); // MongoDB veritabanına bağlanıldı;
+};
+module.exports = mongoConnect; // mongoConnect fonksiyonu export edildi
